07b400121091a1a69967d1fca9ebacb4a2d5ee6b
Arto Bendiken <arto@bendiken.net>
2013-03-01 23:00:37 +0100

    Defined the libxsd++ subproject.

---
+++ feature.h
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef XSDXX_FEATURE_H
+#define XSDXX_FEATURE_H
+
+namespace xsd {
+}
+
+#endif /* XSDXX_FEATURE_H */
---
+++ module.h
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef XSDXX_MODULE_H
+#define XSDXX_MODULE_H
+
+namespace xsd {
+}
+
+#endif /* XSDXX_MODULE_H */
