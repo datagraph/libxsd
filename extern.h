503bf486362c243f8cae2043afbf61eda5a1438b
Arto Bendiken <arto@bendiken.net>
2013-03-06 19:07:20 +0100

    Added an "extern.h" C header for each of the subproject libraries.

---
+++ extern.h
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef XSDXX_EXTERN_H
+#define XSDXX_EXTERN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* XSDXX_EXTERN_H */
