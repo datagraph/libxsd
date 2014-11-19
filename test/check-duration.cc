7ee85964962433302fb9cd7f56e9ea41bd950f3a
Arto Bendiken <arto@bendiken.net>
2014-11-19 05:43:59 +0100

    Renamed test harness files for better `make` target names.

---
+++ test/check-duration.cc
@@ -0,0 +1,13 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "catch.hpp"
+
+#include <xsd++/duration.h> /* for xsd::duration */
+
+TEST_CASE("test_duration") {
+  // TODO
+}
