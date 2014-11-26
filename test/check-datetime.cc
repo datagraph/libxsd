4c1f7ed1622d28f628e019b3aa9987228afe05b4
Arto Bendiken <arto@bendiken.net>
2014-11-26 18:34:18 +0100

    Added basic unit tests for xsd:dateTime boundary conditions.

--- test/check-datetime.cc
+++ test/check-datetime.cc
@@ -9,5 +9,7 @@
 #include <xsd++/datetime.h> /* for xsd::datetime */
 
 TEST_CASE("test_datetime") {
-  // TODO
+  REQUIRE(xsd::datetime::parse("1970-01-01T00:00:00Z") == 0);                 /* epoch    */
+  REQUIRE(xsd::datetime::parse("2038-01-19T03:14:07Z") == 2147483647000000);  /* (2^31)-1 */
+  REQUIRE(xsd::datetime::parse("1901-12-13T20:45:52Z") == -2147483648000000); /* -(2^31)  */
 }
